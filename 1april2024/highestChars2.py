bc79ddf2c32cf9b6fbeb000b4c84b73b89938999
omkar <shelkeom230@gmail.com>
2024-04-01 17:53:54 +0530

    commit 2 1 april

---
+++ 1april2024/highestChars2.py
@@ -0,0 +1,9 @@
+string=input("enter a string: ")
+lst=[]
+
+for data in string:
+    if data not in lst:
+        lst.append(data)
+
+for char in lst:
+    print(char ," ",string.count(char),"times")
